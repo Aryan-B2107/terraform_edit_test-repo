e2ca66513d59cf049942dac41832cce9a6dbc677
Aryan-B2107 <aryanbalikondwar@gmail.com>
2026-07-07 20:50:04 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,7 +1,38 @@
-resource "azurerm_linux_virtual_machine" "analytics" {
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-  name = "analytics-vm"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
-  size = "Standard_D32s_v5"
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "linux-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
 
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_password                  = "Password@1234"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
