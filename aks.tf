0f10149de49d74a074422757093d89d340a9da1d
Aryan-B2107 <aryanbalikondwar@gmail.com>
2026-07-07 20:49:39 +0530

    Create aks.tf

---
+++ aks.tf
@@ -0,0 +1,20 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "demo-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "demoaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
